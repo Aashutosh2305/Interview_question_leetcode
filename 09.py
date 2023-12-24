0e195f2937edfd516f98633a8a106e05e5819af0
ashutosh <mishraatul737@gmail.com>
2023-12-24 17:39:28 +0530

    Subsequence

---
+++ 09.py
@@ -0,0 +1,16 @@
+class solution:
+
+    def subsequence(self, s:str, t:str):
+
+        s_index  =  t_index = 0
+
+        while s_index < len(s) and t_index < len(t):
+
+            if s[s_index] == t[t_index]:
+                s_index += 1
+
+            t_index += 1
+
+        return s_index == len(s)
+
+
